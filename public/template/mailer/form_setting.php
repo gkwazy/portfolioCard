557119dbc8c47c29d9b072d6023847d4a4253c67
gkwazy <noreply>
2018-09-10 22:55:01 -0600

    better

--- public/template/mailer/form_setting.php
+++ public/template/mailer/form_setting.php
@@ -2,7 +2,7 @@
 	/*Form settings*/
 	$subj = "New message from the site 'vCard & Resume Template'"; //letter subject
 	$to = 'gkwazy@gmail.com'; // Enter Your E-mail
-	$from = 'admin@gkwazy.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
+	$from = 'gkwazysite@gmail.com'; // Admin e-mail
+	$fromName = 'gkwazys site'; // Your company name
 	$charset = 'UTF-8';
 ?>
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require("express");
-var db = require("./models");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-var PORT = process.env.PORT || 3000;
 var app = express();
+var PORT = 3000;
 
-db.sequelize.sync().then(function () {
-    app.listen(PORT, function () {
-        console.log("Port %s is now open for data", PORT);
-    });
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public/template'));
+
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/template/index.html"));
+});
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
